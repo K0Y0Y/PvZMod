bc541bd3fa3bd075f6a9deab2fca8b8394afedc8
Mario Coyoy <m4rio928@gmail.com>
2025-06-02 20:57:17 -0600

    finished conehead zombie

--- NPCs/Zombies/ConeheadZombie.cs
+++ NPCs/Zombies/ConeheadZombie.cs
@@ -4,7 +4,7 @@ using Terraria.ID;
 using Microsoft.Xna.Framework;
 using Terraria.ModLoader;
 using Terraria.GameContent.Bestiary;
-using Terraria.GameContent.ItemDropRules;
+// using Terraria.GameContent.ItemDropRules;
 
 namespace PvZMOD.NPCs.Zombies
 {
@@ -14,14 +14,12 @@ namespace PvZMOD.NPCs.Zombies
         {
             WALKING, //0-6m,14-20
             EATING, //7-13,21-27
-            // DYING //x-x
         }
         public enum armorStatus : byte
         {
             GOOD, // 0-13
             DAMAGED, //13-27
             RUINED, //28-41
-            NO_ARMOR //x-x
         }
         public npcStatus zombieStatus = npcStatus.WALKING;
         public armorStatus coneStatus = armorStatus.GOOD;
@@ -34,14 +32,20 @@ namespace PvZMOD.NPCs.Zombies
         private int walkingFrameEnd = 6;
         private int eatingFrameStart = 7;
         private int eatingFrameEnd = 13;
+
         private int walkingDamagedFrameStart = 14;
         private int walkingDamagedFrameEnd = 20;
         private int eatingDamagedFrameStart = 21;
         private int eatingDamagedFrameEnd = 27;
 
+        private int walkingRuinedFrameStart = 28;
+        private int walkingRuinedFrameEnd = 34;
+        private int eatingRuinedFrameStart = 35;
+        private int eatingRuinedFrameEnd = 41;
+
         public override void SetStaticDefaults()
         {
-            Main.npcFrameCount[Type] = 28;
+            Main.npcFrameCount[Type] = 42;
 
             NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers()
             {
@@ -60,7 +64,7 @@ namespace PvZMOD.NPCs.Zombies
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath2;
             NPC.value = 50f;
-            NPC.knockBackResist = .75f;
+            NPC.knockBackResist = .25f;
             NPC.aiStyle = -1;
             AIType = NPCID.Zombie;
             // Banner = Item.NPCtoBanner(NPCID.Zombie);
@@ -81,20 +85,21 @@ namespace PvZMOD.NPCs.Zombies
             if (NPC.life <= 0)
             {
                 Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("Conehead").Type, 1f);
-                // NPC.netUpdate = true;
+                NPC.netUpdate = true;
                 return;
             }
-            if (NPC.life <= (NPC.lifeMax * 0.5))
+
+            if (NPC.life <= (NPC.lifeMax * 0.5) && !coneStatus.Equals(armorStatus.RUINED))
             {
                 coneStatus = armorStatus.RUINED;
-                // NPC.netUpdate = true;
+                NPC.netUpdate = true;
                 return;
             }
-            if (NPC.life <= (NPC.lifeMax * 0.75))
+
+            if (NPC.life <= (NPC.lifeMax * 0.75) && !coneStatus.Equals(armorStatus.DAMAGED))
             {
                 coneStatus = armorStatus.DAMAGED;
-                // NPC.netUpdate = true;
-                return;
+                NPC.netUpdate = true;
             }
         }
 
@@ -103,7 +108,7 @@ namespace PvZMOD.NPCs.Zombies
         {
             Player target = Main.player[NPC.target];
 
-            if (Vector2.Distance(NPC.Center, target.Center) <= 24f)
+            if (Vector2.Distance(NPC.Center, target.Center) <= 28f)
             {
                 NPC.aiStyle = -1;
                 NPC.velocity.X = 0f;
@@ -156,8 +161,8 @@ namespace PvZMOD.NPCs.Zombies
                             statusFrameEnd = walkingDamagedFrameEnd; // 20
                             break;
                         case armorStatus.RUINED:
-                            break;
-                        case armorStatus.NO_ARMOR:
+                            statusFrameStart = walkingRuinedFrameStart; // 28
+                            statusFrameEnd = walkingRuinedFrameEnd; // 34
                             break;
                         default:
                             break;
@@ -176,13 +181,14 @@ namespace PvZMOD.NPCs.Zombies
                             statusFrameEnd = eatingDamagedFrameEnd; // 27
                             break;
                         case armorStatus.RUINED:
-                            break;
-                        case armorStatus.NO_ARMOR:
+                            statusFrameStart = eatingRuinedFrameStart; //35
+                            statusFrameEnd = eatingRuinedFrameEnd; // 41
                             break;
                         default:
                             break;
                     }
                 }
+
                 // animation frame
                 if (NPC.frame.Y < (statusFrameStart * frameHeight) || NPC.frame.Y > (statusFrameEnd * frameHeight))
                     NPC.frame.Y = statusFrameStart * frameHeight;
@@ -191,15 +197,13 @@ namespace PvZMOD.NPCs.Zombies
 
         public override void ModifyNPCLoot(NPCLoot npcLoot)
         {
-            npcLoot.Add(ItemDropRule.Common(ItemID.Shackle, 50));
-            npcLoot.Add(ItemDropRule.Common(ItemID.ZombieArm, 250));
         }
 
         public override void OnKill()
         {
             if (Main.netMode != NetmodeID.MultiplayerClient)
             {
-                NPC.NewNPC(NPC.GetSource_Death(), (int)NPC.Center.X, (int)NPC.Center.Y, ModContent.NPCType<BasicZombie>());
+                NPC.NewNPC(NPC.GetSource_Death(), (int)NPC.Center.X, (int)NPC.Bottom.Y, ModContent.NPCType<BasicZombie>());
             }
         }
     }
